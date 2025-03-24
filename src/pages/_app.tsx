08453c7eab203f2e7d3bd843e753bcde194b33d8
anyelithj <anyelithjc@gmail.com>
2025-03-24 17:52:44 -0500

    test commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,3 @@
-// import { store } from "@/store/store";
-// import { AppProps } from "next/app";
-// import { Provider } from "react-redux";
-// import '../styles/globals.css';
-
-
-// function MyApp({ Component, pageProps }: AppProps) {
-//   <Provider store={store}>
-//      <Component {...pageProps}/>
-//   </Provider>
-// }
-
-// export default MyApp;
 import type { AppProps } from 'next/app';
 import { Provider } from 'react-redux';
 import { store } from '@/store/store';
